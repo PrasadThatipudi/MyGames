c3bebb800a00ca5e636e0354eb19fbddafe12b5a
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2024-11-21 16:09:53 +0530

    Update findThePath.js

--- findThePath.js
+++ findThePath.js
@@ -40,23 +40,26 @@ function game(mineLength, mineWidth) {
   let xPosition = mineLength;
   let yPosition = mineWidth;
 
-  while (true) {
+  while (!(xPosition === 1 && yPosition === 1)) {
     console.clear();
     console.log(getMineField(mineLength, mineWidth, xPosition, yPosition));
-
+    
     const direction = +prompt("Enter the direction: ", "0");
-
+    
     if (direction === LEFT || direction === RIGHT) {
       xPosition = xPosition + getOffSet(direction, 1);
     }
-
+    
     if (direction === UP || direction === DOWN) {
       yPosition = yPosition + getOffSet(direction, 3);
     }
   }
+  
+  console.clear();
+  console.log(getMineField(mineLength, mineWidth, xPosition, yPosition));
 }
 
 const mineLength = 5;
 const mineWidth = 5;
 
-game(mineLength);
+game(mineLength, mineWidth);
