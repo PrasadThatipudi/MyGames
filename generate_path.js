b89f8ed2a64e2fbbd9151648e729bfce1d2da108
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2024-11-24 14:01:25 +0530

    Update generate_path.js

--- generate_path.js
+++ generate_path.js
@@ -1,8 +1,41 @@
-function generatePath(mineLength, mineWidth) {
-  console.log(mineLength, mineWidth);
+function getRandomNumber(min, max) {
+  return min + Math.round(Math.random() * (max - min));
 }
 
-const mineLength = 0;
-const mineWidth = 0;
+function generatePath(mineLength, mineWidth, xInitial, yInitial) {
+  let distance = xInitial + yInitial;
+  let prevX = xInitial;
+  let prevY = yInitial;
+  let path = "";
 
-console.log(generatePath(mineLength, mineWidth));
+  while (distance <= mineLength + mineWidth) {
+    const xPosition = getRandomNumber(prevX, mineLength);
+    const yPosition = getRandomNumber(prevY, prevX + 1);
+
+    console.log(xPosition, yPosition);
+
+
+    if ((xPosition + yPosition) === distance) {
+      path += "" + xPosition + yPosition + "-";
+      console.log(path);
+
+      if (yPosition === mineWidth) {
+        return path;
+      }
+
+      prevX = xPosition;
+      prevY = yPosition;
+
+      distance++;
+    }
+  }
+
+  return path;
+}
+
+const mineLength = 5;
+const mineWidth = 5;
+const xInitial = 1;
+const yInitial = 1;
+
+console.log(generatePath(mineLength, mineWidth, xInitial, yInitial));
