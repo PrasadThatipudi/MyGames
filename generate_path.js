26a1ca351f9ac3f63b5600374b47db3cedce3e00
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2024-11-24 12:34:41 +0530

    Update generate_path.js

--- generate_path.js
+++ generate_path.js
@@ -1,1 +1,8 @@
-console.log("generate path");
+function generatePath(mineLength, mineWidth) {
+  console.log(mineLength, mineWidth);
+}
+
+const mineLength = 0;
+const mineWidth = 0;
+
+console.log(generatePath(mineLength, mineWidth));
