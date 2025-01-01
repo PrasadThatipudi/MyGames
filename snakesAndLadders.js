83f69a72fb3f677ce469a6e330b7b680f27b648a
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-01-01 12:21:44 +0530

    removes slice and repeat functions

--- snakesAndLadders.js
+++ snakesAndLadders.js
@@ -1,112 +1,87 @@
-function repeat(string, noOfRepetitions) {
-if (noOfRepetitions < 1) {
-  return "";
-}
-
-return string + repeat(string, noOfRepetitions - 1);
-}
-
-function getSlice(text, start, end) {
-if (start === end + 1) {
-  return "";
-}
-
-return text[start] + getSlice(text, start + 1, end);
-}
-
-function min(num1, num2) {
-return num1 < num2 ? num1 : num2;
-}
-
-function max(num1, num2) {
-return num1 > num2 ? num1 : num2;
-}
-
-function slice(text, start, end) {
-return getSlice(text, max(start, 0), min(end, text.length - 1));
-}
-
 function rollTheDice() {
-return Math.ceil(Math.random() * 6);
+  return Math.ceil(Math.random() * 6);
 }
 
 function getNumberSymbol(number) {
-const numberSymbols = "0️⃣1️⃣2️⃣3️⃣4️⃣5️⃣6️⃣7️⃣8️⃣9️⃣";
-const start = number * 3;
+  const numberSymbols = "0️⃣1️⃣2️⃣3️⃣4️⃣5️⃣6️⃣7️⃣8️⃣9️⃣";
+  const start = number * 3;
 
-return slice(numberSymbols, start, start + 2);
+  return numberSymbols.slice(start, start + 2);
 }
 
 function getNumberInString(number, stringLength) {
-let stringNum = "" + number;
-const noOfZerosNeeded = stringLength - stringNum.length;
+  let stringNum = "" + number;
+  const noOfZerosNeeded = stringLength - stringNum.length;
 
-stringNum = repeat("0", noOfZerosNeeded) + stringNum;
+  stringNum = "0".repeat(noOfZerosNeeded) + stringNum;
 
-return stringNum;
+  return stringNum;
 }
 
 function _updateString(string, updateWith, from, index, charIndex) {
-if (index === string.length) {
-  return "";
-}
+  if (index === string.length) {
+    return "";
+  }
 
-if (index === from && charIndex !== updateWith.length) {
-  return updateWith[charIndex] + 
-  _updateString(string, updateWith, from + 1, index + 1, charIndex + 1);
-}
+  if (index === from && charIndex !== updateWith.length) {
+    return (
+      updateWith[charIndex] +
+      _updateString(string, updateWith, from + 1, index + 1, charIndex + 1)
+    );
+  }
 
-return string[index] + 
-_updateString(string, updateWith, from, index + 1,charIndex);
+  return (
+    string[index] +
+    _updateString(string, updateWith, from, index + 1, charIndex)
+  );
 }
 
 function updateString(string, updateWith, from) {
-return _updateString(string, updateWith, from, 0, 0);
+  return _updateString(string, updateWith, from, 0, 0);
 }
 
 function isStringPresentAt(string, otherString, stringStart) {
-for (let index = 0; index < otherString.length; index++) {
-  const stringIndex = stringStart + index;
+  for (let index = 0; index < otherString.length; index++) {
+    const stringIndex = stringStart + index;
 
-  if (string[stringIndex] !== otherString[index]) {
-    return false;
+    if (string[stringIndex] !== otherString[index]) {
+      return false;
+    }
   }
-}
 
-return true;
+  return true;
 }
 
 function getSubStringIndexAt(string, otherString, stringIndex) {
-if (stringIndex > string.length - 1) {
-  return -1;
-}
+  if (stringIndex > string.length - 1) {
+    return -1;
+  }
 
-if (isStringPresentAt(string, otherString, stringIndex)) {
-  return stringIndex;
-}
+  if (isStringPresentAt(string, otherString, stringIndex)) {
+    return stringIndex;
+  }
 
-return getSubStringIndexAt(string, otherString, stringIndex + 1);
+  return getSubStringIndexAt(string, otherString, stringIndex + 1);
 }
 
 function subStringIndexAt(string, otherString) {
-if (otherString.length === 0) {
-  return false;
-}
+  if (otherString.length === 0) {
+    return -1;
+  }
 
-return getSubStringIndexAt(string, otherString, 0);
+  return getSubStringIndexAt(string, otherString, 0);
 }
-
 
 function printDiceValue(playerNo, diceValue) {
-console.log("Player " + playerNo + " got " + getNumberSymbol(diceValue));
+  console.log("Player " + playerNo + " got " + getNumberSymbol(diceValue));
 }
 
 function getDiceValue(playerNo) {
-if (prompt("roll the dice-player " + playerNo + ": ", "press enter")) {
-  const dice = rollTheDice();
-  printDiceValue(playerNo, dice);
-  return dice;
-}
+  if (prompt("roll the dice-player " + playerNo + ": ", "press enter")) {
+    const dice = rollTheDice();
+    printDiceValue(playerNo, dice);
+    return dice;
+  }
 }
 
 const TARGET = 100;
@@ -124,17 +99,17 @@ function printPlayerPosition(playerNo, score) {
 }
 
 const LADDERS_START = "004-029-014-022-041-054";
-const LADDERS_END = "056-031-055-058-079-088";
+const LADDERS_TOP = "056-031-055-058-079-088";
 
 function getLadderIndex(playerPosition) {
   return subStringIndexAt(LADDERS_START, getNumberInString(playerPosition, 3));
 }
 
-const SNAKES_START = "028-037-048-075-094-096";
-const SNAKES_END = "010-003-016-032-071-042";
+const SNAKES_MOUTH = "028-037-048-075-094-096";
+const SNAKES_TALE = "010-003-016-032-071-042";
 
 function getSnakeIndex(playerPosition) {
-  return subStringIndexAt(SNAKES_START, getNumberInString(playerPosition, 3));
+  return subStringIndexAt(SNAKES_MOUTH, getNumberInString(playerPosition, 3));
 }
 
 function getScore(playerNo, playerPosition) {
@@ -142,20 +117,20 @@ function getScore(playerNo, playerPosition) {
   playerPosition += isScoreExeeded(playerPosition + dice) ? 0 : dice;
 
   const ladderIndex = getLadderIndex(playerPosition);
-    
+
   if (ladderIndex !== -1) {
-    playerPosition = +slice(LADDERS_END, ladderIndex, ladderIndex + 2);
+    playerPosition = LADDERS_TOP.slice(ladderIndex, ladderIndex + 2);
 
     printPlayerPosition(playerNo, playerPosition);
     console.log("\nYou got a 🪜");
 
     return playerPosition;
   }
-   
+
   const snakeIndex = getSnakeIndex(playerPosition);
 
   if (snakeIndex !== -1) {
-    playerPosition = +slice(SNAKES_END, snakeIndex, snakeIndex + 2);
+    playerPosition = +slice(SNAKES_TALE, snakeIndex, snakeIndex + 2);
 
     printPlayerPosition(playerNo, playerPosition);
     console.log("\nCongrats! You caught by 🐍");
@@ -169,30 +144,30 @@ function getScore(playerNo, playerPosition) {
 }
 
 function playGameWith(noOfPlayers) {
-  let scoreBoard = repeat("000", noOfPlayers);
+  let scoreBoard = "000".repeat(noOfPlayers);
   let index = 0;
 
   while (index < scoreBoard.length) {
     if (index === 0) {
-      console.log(repeat("-", 40));
+      console.log("-".repeat(40));
     }
 
-    const playerNo = (index / 3) + 1;
-    const prevPosition = +slice(scoreBoard, index, index + 2);
+    const playerNo = index / 3 + 1;
+    const prevPosition = +scoreBoard.slice(index, index + 2);
     let curPosition = getNumberInString(getScore(playerNo, prevPosition), 3);
-   
+
     if (isPlayerWon(+curPosition)) {
       return "Congratulations Player " + playerNo + " won the game";
     }
 
     scoreBoard = updateString(scoreBoard, curPosition, index);
-    
+
     index = (index + 3) % scoreBoard.length;
     console.log();
-  }    
+  }
 }
 
-function home() {
+function main() {
   console.log("Welcome!");
 
   const isUserInGoodMood = confirm("Do you want to play this game?");
@@ -205,4 +180,4 @@ function home() {
   return "Bye👋";
 }
 
-console.log(home());
+console.log(main());
